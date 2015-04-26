bc3e7d0e35cbf90c92667fbaffc7fa5b1dfcfd14
Bartosz Dyja <bartosz.dyja@gmail.com>
2015-04-26 22:27:53 +0200

    Added product permissions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,7 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, only: [:update, :destroy, :create]
+  before_action :is_owner, only: [:edit, :update]
+  
   expose(:category)
   expose(:products)
   expose(:product)
@@ -46,5 +49,12 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:title, :description, :price, :category_id)
+  end
+
+  def is_owner
+    if current_user != product.user
+      redirect_to category_product_path(category, product)
+      flash[:error] = 'You are not allowed to edit this product.'    
+    end
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
 
-  validates_presence_of :rating, :user_id, :content
+  validates_presence_of :rating, :user_id, :content	
 end
